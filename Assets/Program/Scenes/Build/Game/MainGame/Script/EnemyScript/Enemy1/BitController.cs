9866a356bcedd1f4b3d2df61d14e0bc51e3db85d
NecroCoffee <ikasumi.514@i.softbank.jp>
2023-09-14 22:18:27 +0900

    Enemy1.Bitの動きをちょっと作った 制御設計はまだ固まってない あいつらが鯖に来なかったから内容が固まらない どうすんだこれ

---
+++ Assets/Program/Scenes/Build/Game/MainGame/Script/EnemyScript/Enemy1/BitController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BitController : MonoBehaviour
+{
+    private GameObject enemy1Object;
+    private Enemy1 enemy1Script;
+
+    [SerializeField]
+    private int enemyState;
+
+    [SerializeField]
+    private GameObject bitBody;
+
+    [SerializeField]
+    private GameObject bitShooter;
+
+    private GameObject target;
+    private Transform targetPosition;
+
+    Vector3 toDirection;//å¸Ç´ì¸ÇÍÇÈ    
+
+    private void LookAtSalmon()
+    {
+        
+
+        
+
+
+        
+
+        toDirection = (targetPosition.transform.position - bitBody.transform.position).normalized;//å¸Ç´èoÇ∑
+        bitBody.transform.rotation = Quaternion.FromToRotation(Vector3.up, toDirection);//QuaternionÇ≈å¸Ç≠
+    }
+
+    private void Awake()
+    {
+        target = GameObject.FindWithTag("Salmon");
+        targetPosition = target.transform;        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
